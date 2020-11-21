38b65019c612c308d351389978831b807fba4bc5
Roberto-Livi <robertomlivi@outlook.com>
2020-11-20 23:38:36 -0500

    adding autdependency injections into the appropriate classes and methods

--- java-spring-demo/src/main/java/com/example/javaspringdemo/api/PersonController.java
+++ java-spring-demo/src/main/java/com/example/javaspringdemo/api/PersonController.java
@@ -2,11 +2,13 @@ package com.example.javaspringdemo.api;
 
 import com.example.javaspringdemo.model.Person;
 import com.example.javaspringdemo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PersonController {
 
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
--- java-spring-demo/src/main/java/com/example/javaspringdemo/dao/FakePersonDataAccessService.java
+++ java-spring-demo/src/main/java/com/example/javaspringdemo/dao/FakePersonDataAccessService.java
@@ -1,11 +1,13 @@
 package com.example.javaspringdemo.dao;
 
 import com.example.javaspringdemo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
